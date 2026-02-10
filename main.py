0e8386e1640841b6facaa5b8c6d1a09dc9788b9c
talvasconcelos <talvasconcelos@gmail.com>
2026-02-10 10:04:39 +0000

    Migrate project to uv package manager

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import launcher
 import logging
 
-logging.basicConfig(level=logging.INFO)
 
-# Launch the desired bot with the parameters defined in the configuration.yml file
-bot = launcher.bot()
-logging.info(bot)
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # Launch the desired bot with the parameters defined in the configuration.yml file
+    bot = launcher.bot()
+    logging.info(bot)
+
+
+if __name__ == "__main__":
+    main()
